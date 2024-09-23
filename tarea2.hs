3dd4cb7efc0b668f12c7449ccd4752a1a36b5cb2
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-09-23 11:21:13 -0300

    Actualizar tarea2.hs

--- tarea2.hs
+++ tarea2.hs
@@ -60,7 +60,9 @@ sumap2 :: (N -> Bool) -> N -> N
 
 sumap2 p m n | n > m = 0
               | not p m  = sumap2 p (m+1) n
-              | otherwise = m *m + sumap2 p (m+1) n
+              | otherwise = m *m + sumap2 p (m+1) 
+
+todosp :: (N-> Bool)
 
 
 existe.p :: (N-> Bool) -> N -> N -> Bool 
