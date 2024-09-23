128005e6e0cc0b4c323104314a12501e367d998d
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-09-23 09:17:47 -0300

    Cambio en  tarea2.hs

--- tarea2.hs
+++ tarea2.hs
@@ -6,3 +6,10 @@ sumaentre :: N -> N -> N
 sumaentre m n | n > m = 0
               | otherwise m + sumaentre(m+1) n
 
+--Funcion sumaentre.f
+
+sumaentref :: N -> N -> f -> N
+
+
+sumaentref m n | m =0 || n = 0 = 0
+               | otherwise f m + sumaentref(f m+1) n 
