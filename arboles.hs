f70e36144b46273bc7b9c16a10edda9930594dfa
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-10-24 09:46:58 -0300

    Actualizar arboles.hs

--- arboles.hs
+++ arboles.hs
@@ -1,1 +1,17 @@
-arbol :: Integer -> Integer
+data AB a = Hoja a | Nodo a (AB a) (AB a)
+
+
+altura :: AB a - Int
+--Ejercicio 1
+
+altura(Node x izq der) | Node x izq > Node x der = length Node x izq
+                       | otherwise = length Node x der
+
+
+
+
+
+
+
+--Ejercicio4
+isOrder :: ABB a -
