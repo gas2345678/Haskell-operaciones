3fb70c69891995e6eaa2842a102bf89b442eac7c
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-10-28 09:37:36 -0300

    Update Tarea3.hs

--- Tarea3.hs
+++ Tarea3.hs
@@ -1,1 +1,10 @@
 
+data file = String 
+
+
+data FS = A file | Dir file [FS]
+
+
+
+1- let filesystem = Dir raiz [dir Musica [dir Jazz [ A mumbles.mp3] dir rock [ A clones.mp3 A baja.mp3 A clara.mp3] clara.mp3] A notas.txt  dir ort [dir obls [A p2.txt A p2.iar fc.hs] A notas.txt] dir sys [A sys.txt dir sys []]]
+
