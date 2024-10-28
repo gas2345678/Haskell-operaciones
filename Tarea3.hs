46d505f0d5164c4f7dcce2578de7e4b580d0f15a
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-10-28 09:45:24 -0300

    Update Tarea3.hs

--- Tarea3.hs
+++ Tarea3.hs
@@ -8,3 +8,16 @@ data FS = A file | Dir file [FS]
 
 1- let filesystem = Dir raiz [dir Musica [dir Jazz [ A mumbles.mp3] dir rock [ A clones.mp3 A baja.mp3 A clara.mp3] clara.mp3] A notas.txt  dir ort [dir obls [A p2.txt A p2.iar fc.hs] A notas.txt] dir sys [A sys.txt dir sys []]]
 
+
+
+
+
+
+3- contenido :: FS -> [Nombre]
+contenido 
+
+
+4 - CantA :: FS -> Int 
+
+  CantA dir |  
+
