7b230e1e475bd950befb5091b866c7cff75af39f
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-11-18 10:41:22 -0300

    Actualizar Tarea4.hs

--- Tarea4.hs
+++ Tarea4.hs
@@ -4,6 +4,10 @@ type Vertice = Int
 type Arista = (Vertice,Vertice)
 data Grafo = [(Arista,[Vertice])]
 
+type Ciudad = String
+
+
+
 
 
 
@@ -17,6 +21,10 @@ let visitados = []
 
 recorrido = [(1,0),[(2,2),(4,3),(6,2),(6,5)]
 recorrido2 = [(1,0),(3,1),(5,5),(6,7)]
+
+let mapavuelos = [('Newcastle',0),('Leeds',4),('Liverpool',6),('Sheffield',7)]
+
+
 
 
 
