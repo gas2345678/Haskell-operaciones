078df58b5dd3601e4c3fc93153cca60ae5a8f89a
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-11-18 10:49:54 -0300

    Actualizar Tarea4.hs

--- Tarea4.hs
+++ Tarea4.hs
@@ -3,8 +3,9 @@
 type Vertice = Int
 type Arista = (Vertice,Vertice)
 data Grafo = [(Arista,[Vertice])]
+type Costo = Int
 
-data Vuelo (String,Integer)
+data Vuelo (String,Costo)
 data Mapavuelos = [Vuelo,(Vuelo)]
 
 
@@ -24,11 +25,14 @@ let visitados = []
 recorrido = [(1,0),[(2,2),(4,3),(6,2),(6,5)]
 recorrido2 = [(1,0),(3,1),(5,5),(6,7)]
 
-let mapavuelos = [('Newcastle',0),('Leeds',4),('Liverpool',6),('Sheffield',7)]
+let m = [('Newcastle',0),('Leeds',4),('Liverpool',6),('Sheffield',7)]
 
 
 ciudades :: Mapavuelos -> [Ciudad]
 ciudades ((x,xs) :xss) = [] : x : ciudades xss
+
+initialCost :: [Ciudad] -> Costos
+
 
 
 
