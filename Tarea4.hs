a28a8929fea24ea29d2cf78710da83e9b6ba9bcc
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-11-18 09:52:34 -0300

    Actualizar Tarea4.hs

--- Tarea4.hs
+++ Tarea4.hs
@@ -1,14 +1,21 @@
 --Ejercicio4
 --Grafos
+type Vertice = Int
+type Arista = (Vertice,Vertice)
+data Grafo = [(Vertice,[Arista])]
 
 
 
 
 --Algorimo Djkastra
 
+let grafo = 
+
 
 
 let visitados = []
+
+
 
 
 
