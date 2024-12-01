496febc77ab4be974dbea24480f73c0386d1ef61
gas2345678 <82404763+gas2345678@users.noreply.github.com>
2024-12-01 19:09:45 -0300

    Update Tarea4.hs

--- Tarea4.hs
+++ Tarea4.hs
@@ -28,14 +28,12 @@ let visitadas = []
 
 --Algorimo Djkastra
 
-let grafo = [(1,[2,3]),(2,[4]),(3,[4,5]),(5,[6]),[(4,[6]),(6,[])]
+
 
 
 
 let visitados = []
 
-recorrido = [(1,0),[(2,2),(4,3),(6,2),(6,5)]
-recorrido2 = [(1,0),(3,1),(5,5),(6,7)]
 
 let m = [('Newcastle',0),('Leeds',4),('Liverpool',6),('Sheffield',7)]
 
