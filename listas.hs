f77487b5393de1a3cdcaae05f546478c43328b23
gas2345678 <gtno@gmail.com>
2025-10-10 13:00:21 -0300

    new changes

--- listas.hs
+++ listas.hs
@@ -18,7 +18,8 @@ type Strings = [String]
 type Bools = [Bool]
 
 
---lista de listas de enteros(Funciona exactamente igual que una matriz)
+--lista de listas de enteros.
+--Funciona exactamente igual a una matriz ya que se trata de una lista de numeros enteros que puede contener otras listas de enteros dentro de ella.
 type Listas = [[Integer]]
 
 
@@ -34,6 +35,32 @@ sumalista :: NS -> Integer
 sumalista [] = 0
 --Si no esta vacia, sumara el primer elemento con la suma del resto
 sumalista (x:xs) = x + sumalista xs
+
+
+
+--Haskell tambien permite el acceso a distintas funciones predefinidas para listas como hallar el primer elemento o el resto de una lista
+
+
+-- Tenemos la funcion "head" que devuelve el primer elemento de una lista
+
+--Ejemplo: 
+
+
+primerelemento :: NS -> Integer
+primerelemento xs = head xs
+
+
+
+-- O la funcion "tail" que devuelve el resto de la lista sin incluir el primer elemento
+
+
+restolista :: NS -> NS 
+
+--Como pueden ver, no devolvera un solo elemento, sino que devolvera una lista con el resto de los elementos de la lista original
+
+
+restolista xs = tail xs 
+
 
 
 
